e8a31235811d91e15aee16f47cdb20f1a9e48b24
ErickYar <erickyb1985@gmail.com>
2023-12-04 13:00:46 -0500

    creando el archivo urls, views y forms

--- app/views.py
+++ app/views.py
@@ -1,31 +1,34 @@
 from django.shortcuts import render
 from .forms import Alumno
+
 # Create your views here.
+
+
 def registrar(request):
-    if request.method=="POST":
-        # crear objetos de la clase alumno
-        form=Alumno(request.POST)
+    if request.method == "POST":
+        # crear objeto de la clase alumno
+        form = Alumno(request.POST)
         if form.is_valid():
-            #recuperar datos del la pagina html y asignar valores a variable
-            código=form.cleaned_data["código"]
-            nombre=form.cleaned_data["nombre"]
-            curso=form.cleaned_data["curso"]
-            parcial=form.cleaned_data["parcial"]
-            practica=form.cleaned_data["practica"]
-            final=form.cleaned_data["final"]
-            #calcular promedio
-            promedio=(parcial+practica+2*final)/4
-            if promedio>=14:
-                estado="Aprobado"
-            elif promedio>=10  and promedio<14:
-                estado="Sustitutos"
+            # recuperar datos del la pagina html y asignar valores a variables
+            código = form.cleaned_data["código"]
+            nombre = form.cleaned_data["nombre"]
+            curso = form.cleaned_data["curso"]
+            parcial = form.cleaned_data["parcial"]
+            practica = form.cleaned_data["practica"]
+            final = form.cleaned_data["final"]
+        
+            promedio = (parcial+practica+2*final)/4
+            if promedio >= 14:
+                estado = "Aprobado"
+            elif promedio >= 10 and promedio < 14:
+                estado = "Sustitutorio"
             else:
-                estado="Desaprobado"
-                contexto={'Código':código, 'nombre':nombre,
-                'curso':curso,'parcial':parcial,'practica':practica,'final':final,'promedio':promedio,'estado':estado}
-            return render(request,'app/boleta.html',contexto)
+                estado = "Desaprobado"
+            contexto = {'código': código, 'nombre': nombre, 'curso': curso,
+                        'parcial': parcial, 'practica': practica, 'final': final, 'promedio': promedio, 'estado': estado}
+            return render(request, 'app/boleta.html', contexto)
     else:
-        #crear objeto  de la clase alumno
-        form=Alumno()
-        contexto={'form':form}
-        return render(request,'app/registrar.html',contexto)
+        # crear objeto de la clase alumno
+        form = Alumno()
+    contexto = {'form': form}
+    return render(request, 'app/registrar.html', contexto)
